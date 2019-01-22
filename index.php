bd309828b7516cc25cf9189a7a853bc1523fa3e4
Jacot-dit-Montandon <ludovic.jctdt@eduge.ch>
2019-01-22 13:26:44 +0100

    Pop-up de connexion

--- index.php
+++ index.php
@@ -36,33 +36,62 @@
                         <a class="text-muted" href="#">Subscribe</a>
                     </div>
                     <div class="col-4 text-center">
-                        <a class="blog-header-logo text-dark" href="#">Large</a>
+                        <a class="blog-header-logo text-dark" href="#">M306</a>
                     </div>
+
                     <div class="col-4 d-flex justify-content-end align-items-center">
                         <a class="text-muted" href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
                         </a>
-                        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#loginModal" data-whatever="@mdo">Connexion</button>
                     </div>
                 </div>
             </header>
 
-            <div class="nav-scroller py-1 mb-2">
-                <nav class="nav d-flex justify-content-between">
-                    <a class="p-2 text-muted" href="#">World</a>
-                    <a class="p-2 text-muted" href="#">U.S.</a>
-                    <a class="p-2 text-muted" href="#">Technology</a>
-                    <a class="p-2 text-muted" href="#">Design</a>
-                    <a class="p-2 text-muted" href="#">Culture</a>
-                    <a class="p-2 text-muted" href="#">Business</a>
-                    <a class="p-2 text-muted" href="#">Politics</a>
-                    <a class="p-2 text-muted" href="#">Opinion</a>
-                    <a class="p-2 text-muted" href="#">Science</a>
-                    <a class="p-2 text-muted" href="#">Health</a>
-                    <a class="p-2 text-muted" href="#">Style</a>
-                    <a class="p-2 text-muted" href="#">Travel</a>
-                </nav>
+            <?php include 'navigation.php'; ?>
+
+            <!-- Modal -->
+            <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Connexion</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="email" class="col-form-label">Email:</label>
+                                    <input type="email" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="col-form-label">Mot de passe:</label>
+                                    <input type="password" class="form-control" id="recipient-name">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                            <button type="button" class="btn btn-primary">Se connecter</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <!-- JS code -->
+            <script src="https://code.jquery.com/jquery-3.1.1.min.js">
+            </script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js">
+            </script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js">
+            </script>
+            <!--modal-->
+            <script>
+                $(document).ready(function () {
+                    $("#loginModal").modal();
+                });
+            </script>
 
             <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
                 <div class="col-md-6 px-0">
